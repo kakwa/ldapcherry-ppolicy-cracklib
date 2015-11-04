9c47bbbb6d6896694d9a4b6d667975e872182a17
kakwa <carpentier.pf@gmail.com>
2015-11-04 07:57:35 +0100

    adding test for dictionary check

--- tests/test_ppolicy.py
+++ tests/test_ppolicy.py
@@ -41,7 +41,7 @@ config_dict = {
     'min_lower': 0,
     'min_digit': 0,
     'min_other': 0,
-    'dict_path': os.getcwd() + '/tests/resources/dict',
+    'dict_path': os.getcwd() + '/tests/resources/cracklib_dict',
 }
 
 config_falsedict = {
@@ -76,70 +76,77 @@ password = 'MzgzMDQw,NDgK3830.'
 
 class TestLib(object):
 
-   def test_base(self):
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': True, 'reason': 'Password ok'}
-       assert exp == ret
+    def test_base(self):
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': True, 'reason': 'Password ok'}
+        assert exp == ret
+ 
+    def test_upper(self):
+        password = 'mzgzmdqw,NDgk3830.'
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password has not enough upper case characters'}
+        assert exp == ret
+ 
+    def test_lower(self):
+        password = 'QWENIHSYT,NDgk3830.'
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password has not enough lower case characters'}
+        assert exp == ret
+ 
+    def test_digits(self):
+        password = 'QWENIHSYT,NDgkqa3p.'
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password has not enough digits'}
+        assert exp == ret
+ 
+    def test_other(self):
+        password = 'MzgzMDQw,NDgK3830p'
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password has not enough non alphanumeric characters'}
+        assert exp == ret
+ 
+    def test_other(self):
+        password = 'Mzaw,NDgK38p,'
+        ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password is too short'}
+        assert exp == ret
+ 
+    def test_dictNotAbsPath(self):
+        try:
+            ppolicy = lcppolicy_cracklib.PPolicy(config_errordictnotabspath, cherrypy.log)
+        except Exception as e:
+            assert str(e) == 'dict_path must be absolute, or not declared'
+        else:
+            raise AssertionError("expected an exception")
+ 
+    def test_dictDoesNotExist(self):
+        try:
+            ppolicy = lcppolicy_cracklib.PPolicy(config_errordictpath, cherrypy.log)
+        except OSError:
+            return
+        else:
+            raise AssertionError("expected an exception")
+ 
+    def test_Dict(self):
+        password = 'isaword'
+        ppolicy = lcppolicy_cracklib.PPolicy(config_dict, cherrypy.log)
+        ret = ppolicy.check(password)
+        exp = {'match': False, 'reason': 'Password it is based on a dictionary word'}
+        assert exp == ret
 
-   def test_upper(self):
-       password = 'mzgzmdqw,NDgk3830.'
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': False, 'reason': 'Password has not enough upper case characters'}
-       assert exp == ret
-
-   def test_lower(self):
-       password = 'QWENIHSYT,NDgk3830.'
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': False, 'reason': 'Password has not enough lower case characters'}
-       assert exp == ret
-
-   def test_digits(self):
-       password = 'QWENIHSYT,NDgkqa3p.'
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': False, 'reason': 'Password has not enough digits'}
-       assert exp == ret
-
-   def test_other(self):
-       password = 'MzgzMDQw,NDgK3830p'
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': False, 'reason': 'Password has not enough non alphanumeric characters'}
-       assert exp == ret
-
-   def test_other(self):
-       password = 'Mzaw,NDgK38p,'
-       ppolicy = lcppolicy_cracklib.PPolicy(config, cherrypy.log)
-       ret = ppolicy.check(password)
-       exp = {'match': False, 'reason': 'Password is too short'}
-       assert exp == ret
-
-   def test_dictNotAbsPath(self):
-       try:
-           ppolicy = lcppolicy_cracklib.PPolicy(config_errordictnotabspath, cherrypy.log)
-       except Exception as e:
-           assert str(e) == 'dict_path must be absolute, or not declared'
-       else:
-           raise AssertionError("expected an exception")
-
-   def test_dictDoesNotExist(self):
-       try:
-           ppolicy = lcppolicy_cracklib.PPolicy(config_errordictpath, cherrypy.log)
-       except OSError:
-           return
-       else:
-           raise AssertionError("expected an exception")
-
-   @travis_disabled
-   def test_dictFalse(self):
-       try:
-           ppolicy = lcppolicy_cracklib.PPolicy(config_falsedict, cherrypy.log)
-       except RuntimeError:
-           return
-       else:
-           raise AssertionError("expected an exception")
+    @travis_disabled
+    def test_dictFalse(self):
+        try:
+            ppolicy = lcppolicy_cracklib.PPolicy(config_falsedict, cherrypy.log)
+        except RuntimeError:
+            return
+        else:
+            raise AssertionError("expected an exception")
 
 
